ab88b9a4ba3523ae548885d8b2aacc2a1990c8d2
110v <sgb1616@gmail.com>
2022-02-19 23:36:43 +0900

    feat : fix jsx array elements

--- grid-engine/Content/jsxRenderer.ts
+++ grid-engine/Content/jsxRenderer.ts
@@ -1,6 +1,7 @@
 import { createElement, VirtualElement } from "./Vdom";
 
-function jsx(type: string, options: any, ...childs: (VirtualElement)[]): VirtualElement {
+function jsx(type: string, options: any, ...childs: (VirtualElement|string)[]): VirtualElement {
+    childs = childs.flat();
     const element = createElement(type,options,childs);
     return element;
 }
